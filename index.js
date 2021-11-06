a5a9ebce45d3a8c28687e0ab5b826d403c33a074
smh1988-2 <seamhurley1988@gmail.com>
2021-11-06 13:06:23 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Code your solution here
+// drivers = [
+//     'Bobby', 
+//     'Sammy', 
+//     'Sally',
+//     'Annette', 
+//     'Sarah', 
+//     'Bobby'
+// ]
+
+const driversCity = [
+    {
+        name: 'Sammy',
+        hometown: 'New York' } ,
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+function findMatching(arr, query) {
+    return arr.filter(function(name) {
+        return name.toLowerCase().indexOf(query.toLowerCase()) !== -1
+    })
+}
+
+// console.log(findMatching(drivers, 'Bobby'))
+
+function fuzzyMatch(arr, query) {
+    return arr.filter(function(name) {
+        return name.startsWith(query)
+    })
+}
+
+// console.log(fuzzyMatch(drivers, 'Sa'))
+// console.log(fuzzyMatch(drivers, 'Sal')) 
+
+//this works but only when the original drivers array is commented out? Why? WTF. jfc
+let matchName = (arr, query) => arr.filter(e => e.name === query)
+matchName(driversCity, 'Bobby')
+
+console.log(matchName(driversCity, 'Bobby'))
